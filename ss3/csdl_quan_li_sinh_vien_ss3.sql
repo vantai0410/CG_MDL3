97b6a5a3fc2ea1c0ae72914ba04eea4651389fe3
vantai0410 <vantai04102004@gmail.com>
2024-11-05 20:41:11 +0700

    up

---
+++ ss3/csdl_quan_li_sinh_vien_ss3.sql
@@ -0,0 +1,33 @@
+use quanlysinhvien;
+
+ -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+ select *
+ from student
+ where StudentName like "H%"
+ 
+ -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+ select *
+ from class
+ where month(StartDate) = 12;
+ 
+ -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+ select *
+ from subject
+ where Credit >=3 && Credit <= 5;
+ 
+ -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+ set sql_safe_updates = 0;
+ update student
+ set ClassID = 2
+ where StudentName like "Hung";
+ 
+ select *
+ from student;
+ 
+ -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+ select s.StudentName, sj.SubName, m.Mark
+ from student s
+ inner join mark m on s.StudentId = m.StudentId
+ inner join subject sj on sj.SubId = m.SubId
+ order by m.Mark desc, s.StudentName asc;
+ 
---
+++ ss3/truy_van_demo.sql
@@ -0,0 +1,50 @@
+ use demo;
+ -- 1. Lấy ra thông tin tất cả học viên có lớp học và tên lớp mà các học viên đó đang theo học.
+ select student.*, class.class_name
+ from student 
+ join class on student.class_id = class.class_id;
+ 
+ -- 2. Lấy ra thông tin tất cả học viên (bao gồm có và chưa có lớp) và tên lớp (nếu có) mà các học viên đó đang theo học.
+ select student.*, class.class_name
+ from student 
+ left join class on student.class_id = class.class_id ;
+ 
+ -- 3. Lấy thông tin của các học viên tên “Hai” và 'Huynh’.
+ select *
+ from student
+ where student_name like "%hai" or student_name like "%huynh"; 
+ 
+-- 4. Lấy ra thông tin học viên có điểm lớn hơn 5 .
+select *
+from student
+where student_point > 5;
+
+-- 5. Lấy ra thông tin học viên có họ là “nguyen”
+ select *
+ from student
+ where student_name like "nguyen%";
+ 
+ -- 6. Thông kế số lượng học sinh theo từng loại điểm.
+ select student_point, count(student_point) as so_luong
+ from student
+ group by student_point;
+ 
+ -- 7. Thông kế số lượng học sinh theo điểm và điểm phải lớn hơn 5
+ select student_point, count(student_point) as so_luong
+ from student
+ where student_point > 5
+ group by student_point;
+ 
+ -- 8. Thông kế số lượng học sinh theo điểm lớn hơn 5 và chỉ hiện thị với số lượng >= 2
+  select student_point, count(student_point) as so_luong
+ from student
+ where student_point > 5
+ group by student_point
+ having so_luong >=2;
+ 
+-- 9. Lấy ra danh sách học viên của lớp c1121g1 và sắp xếp tên học viên theo alphabet.
+select *
+from student  
+ join class on student.class_id = class.class_id
+ where class.class_name = "c1121g1"
+ order by student.student_name asc;
